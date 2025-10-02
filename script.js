2cf912c5f9be4ae0ecf98c78422f889ce21089f4
Dorukan <catakd@mef.edu.tr>
2025-10-02 13:46:49 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+
+function operate(num1, num2, operator){
+    switch(operator){
+        case 'add':
+            // add the numbers and update the display
+            break;
+        case 'subtract':
+
+            break;
+        case 'multiply':
+
+            break;
+        case 'divide':
+
+            break;
+    }
+}
+
