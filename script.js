5430acc0b24500108aac1ac9c957192bbfd6f377
Dorukan <catakd@mef.edu.tr>
2025-10-02 20:58:22 +0300

    add some functionality, buggy version

--- script.js
+++ script.js
@@ -17,27 +17,57 @@ function divide(a,b){
 const display = document.getElementById("display");
 const keypad = document.querySelector(".keypad");
 
+let data = {
+        'num1': '',
+        'num2': '',
+        'operator': '',
+    }
+
 keypad.addEventListener('click', function(event){
     if (event.target.tagName !== 'BUTTON') return;
-    const val = event.target.dataset.value;
-    display.textContent += val;
+
+    const val = event.target;
+
+    if(val.className === 'digit'){
+        display.textContent += val.dataset.value;
+        if(data.num1 !== '' && data.operator !== ''){
+           data.num2 = parseFloat(display.textContent); 
+           console.log(data);
+        }else{
+            data.num1 = parseFloat(display.textContent); // num1
+            console.log(data);
+        }
+    }else{
+        if(data.operator !== ''){
+            data.num1 = operate(data.num1, data.num2, data.operator);
+            display.textContent = data.num1;
+            val.dataset.value === 'equal' || function(){ data.operator = val.dataset.value; };
+            console.log(data);
+        }else{
+            data.operator = val.dataset.value;
+            display.textContent = '';
+            console.log(data);
+        }
+    }
+    
+
+    
 });
 
 
 function operate(num1, num2, operator){
     switch(operator){
         case 'add':
-            // add the numbers and update the display
-            display.textContent = add(num1, num2);
+            return add(num1, num2);
             break;
         case 'subtract':
-
+            return subtract(num1, num2);
             break;
         case 'multiply':
-
+            return multiply(num1, num2);
             break;
         case 'divide':
-
+            return divide(num1, num2);
             break;
     }
 }
